f9eda8ecc5b90c7500414102f53902156826c112
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-06 16:46:37 -0700

    all the category endpoints should work (not tested, however)--might be bugs

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,67 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }] 
+    });
+    res.status(200).json(categoryData);
+  }
+  catch (error){
+    res.status(404).send(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try{
+    const categoryDataById = await Category.findByPk(req.params.id,{
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryDataById);
+  }
+  catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try{
+    const newCategory = req.body;
+    await Category.create(newCategory);
+    res.status(200).send('Here is your new category: ' + newCategory);
+  }
+  catch (error){
+    res.status(400).send(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try{
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updatedCategory);
+  }
+  catch (error){
+    res.status(400).send(erorr);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).send('Category successfully deleted!');
+  }
+  catch (error){
+    res.status(404).send(error)
+  }
 });
 
 module.exports = router;
